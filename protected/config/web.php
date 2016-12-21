dcd1bdfa1b9ad71249cf7ab2121166b3903b18d3
Samuel DeSota <me@samdesota.com>
2016-12-21 14:16:45 -0500

    Initial Commit

--- protected/config/web.php
+++ protected/config/web.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
+    'bootstrap' => ['debug'],
+	'modules' => [
+	    'debug' => [
+	        'class' => 'yii\debug\Module',
+	        'allowedIPs' => ['127.0.0.1', '::1'],
+	    ],
+	]
 ];
-
