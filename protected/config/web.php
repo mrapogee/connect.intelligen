e3e0de30d32215595f949cb8535464ebd782b738
Samuel DeSota <me@samdesota.com>
2017-01-09 12:43:46 -0500

    added my ip to whitelist

--- protected/config/web.php
+++ protected/config/web.php
@@ -5,7 +5,7 @@ return [
 	'modules' => [
 	    'debug' => [
 	        'class' => 'yii\debug\Module',
-	        'allowedIPs' => ['127.0.0.1', '::1'],
+	        'allowedIPs' => ['127.0.0.1', '::1', '75.136.77.25'],
 	    ],
 	],
 
