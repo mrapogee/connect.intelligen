6692e640beb791eac19e9a8fb57e144f35c5ffd9
Samuel DeSota <me@samdesota.com>
2017-01-28 18:28:55 -0500

    fixing forms

--- protected/modules/forms/assets/Builder.php
+++ protected/modules/forms/assets/Builder.php
@@ -9,6 +9,8 @@ class Builder extends AssetBundle
     public $sourcePath = '@intelligen/modules/forms/assets/resources';
 
     public $css = [
+        'vendor/font-awesome.css',
+        'vendor/font-awesome-463.css',
         'vendor/ngFormBuilder.css'
     ];
     
